44fe73f7ed3b488c53f037f97edd71f158205991
Abhishek Mundra <abhishek.mundra@cvshealth.com>
2018-03-21 16:51:11 -0500

    updated for root directory unzip

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ fi
 #Zip for shipment, remove if it exists
 rm -f dist.zip
 
-zip -r dist.zip dist/
+zip -r dist.zip dist/*
 if [ $? -ne 0 ]; then
   echo "Zipping the build directory resulted in an error"
   exit 1
