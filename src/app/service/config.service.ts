63200a98b2dc51f79a1e3d1c1754176abf6d3716
u008069 <gaurav.tyagi@cvshealth.com>
2018-04-30 14:05:15 -0500

    Updated input data mapping

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -9,18 +9,17 @@ declare const window: any;
 export class ConfigService {
     public env: string;
     public apiKey: string;
-    public mfaToken: string;
+    public token: string;
     public apiBaseUrl: string;
-    public dsc: string;
-    public sp: string;
-    public vp: string;
-    public ap: string;
-    public digitalFingerPrint: string;
     public participantID: string;
     public participantFirstName: string;
+    // public dsc: string;
+    // public sp: string;
+    // public vp: string;
+    // public ap: string;
 
     ready = new BehaviorSubject(false);
-    public additionalQuery = 'appName=CMK_WEB&channelName=WEB&deviceID=device12345&deviceToken=device12345&deviceType=AND_MOBILE&lineOfBusiness=PBM&serviceCORS=TRUE&version=1.0';
+    // public additionalQuery = 'appName=CMK_WEB&channelName=WEB&deviceID=device12345&deviceToken=device12345&deviceType=AND_MOBILE&lineOfBusiness=PBM&serviceCORS=TRUE&version=1.0';
 
 
     constructor() {
@@ -42,7 +41,7 @@ export class ConfigService {
     }
 
 
-    getRouteFor(serviceRoute: string, noQuery?: boolean) {
+    /*getRouteFor(serviceRoute: string, noQuery?: boolean) {
         let route;
         if (noQuery) {
             route = `${this.apiBaseUrl}${serviceRoute}`;
@@ -50,7 +49,7 @@ export class ConfigService {
             route = `${this.apiBaseUrl}${serviceRoute}?apiKey=${this.apiKey}&mfaToken=${this.mfaToken}&serviceName=${serviceRoute.split('/')[1]}&${this.additionalQuery}`;
         }
         return route;
-    }
+    }*/
 
 
     init: Function = _.debounce(() => {
@@ -61,25 +60,25 @@ export class ConfigService {
                 this.env = data.apiData.env;
                 this.apiKey = data.apiData.apiKey;
                 this.apiBaseUrl = data.apiData.apiBaseUrl;
+                this.token = data.apiData.tokenId;
                 if (this.apiBaseUrl.includes('devservices-west.caremark.com')) {
                     this.apiBaseUrl = `https://devservices-west.caremark.com:11101/`;
                 }
                 this.participantID = data.appData.ParticipantID;
                 this.participantFirstName = data.appData.ParticipantFirstName;
             }
-            console.log('Env => ' + this.env + ' Participant Id => ' + this.participantID + ' Participant Name => ' + this.participantFirstName)
         } catch (e) {
             console.log('config service --> init() :' + e);
         } finally {
-            //this.ready.next(this.validate());
+            this.ready.next(this.validate());
         }
     }, 200);
 
 
     private validate(): boolean {
-        console.log(`env: ${this.env}\napiKey: ${this.apiKey}\nmfaToken: ${this.mfaToken}\napiBaseUrl: ${this.apiBaseUrl}`);
+      console.log('Env => ' + this.env + ' Participant Id => ' + this.participantID + ' Participant Name => ' + this.participantFirstName);
         return !(isNullOrUndefined(this.env) || isNullOrUndefined(this.apiKey)
-            || isNullOrUndefined(this.apiBaseUrl) || isNullOrUndefined(this.mfaToken)) || (!isNullOrUndefined(this.env) && this.env.includes('demo'));
+            || isNullOrUndefined(this.apiBaseUrl) || isNullOrUndefined(this.token)) || (!isNullOrUndefined(this.env) && this.env.includes('demo'));
     }
 
 }
