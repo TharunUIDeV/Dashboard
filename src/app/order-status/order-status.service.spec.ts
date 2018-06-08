d146a9a1b5d58a376f85603ba059beda1a6d21a9
Sridhar Uyyala <sridhar.uyyala@cvshealth.com>
2018-06-08 10:32:33 -0500

    Fix ng test failures

--- src/app/order-status/order-status.service.spec.ts
+++ src/app/order-status/order-status.service.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { OrderStatusService } from './order-status.service';
+import {CaremarkDataService} from '../service/caremark-data.service';
+import {ConfigService} from '../service/config.service';
+import {CaremarkSdkService} from '../service/caremark-sdk.service';
+import {IceSdkService} from '../service/ice-sdk.service';
+import {VordelPbmService} from '../service/vordel-pbm.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {MemberService} from '../service/member.service';
 
 describe('OrderStatusService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [OrderStatusService]
+      providers: [
+        OrderStatusService,
+        CaremarkDataService,
+        ConfigService,
+        MemberService,
+        CaremarkSdkService,
+        IceSdkService,
+        VordelPbmService,
+        HttpClient,
+        HttpHandler,
+      ]
     });
   });
 
-  xit('should be created', inject([OrderStatusService], (service: OrderStatusService) => {
+  it('should be created', inject([OrderStatusService], (service: OrderStatusService) => {
     expect(service).toBeTruthy();
   }));
 });
--- src/app/service/vordel-pbm.service.spec.ts
+++ src/app/service/vordel-pbm.service.spec.ts
@@ -1,15 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { VordelPbmService } from './vordel-pbm.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {ConfigService} from './config.service';
 
 describe('VordelPbmService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [VordelPbmService]
+      providers: [
+        VordelPbmService,
+        HttpClient,
+        HttpHandler,
+        ConfigService
+      ]
     });
   });
 
-  xit('should be created', inject([VordelPbmService], (service: VordelPbmService) => {
+  it('should be created', inject([VordelPbmService], (service: VordelPbmService) => {
     expect(service).toBeTruthy();
   }));
 });
