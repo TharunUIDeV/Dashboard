eb3610a243accf11d5eb71278bf42023e3f7f50f
Sridhar Uyyala <sridhar.uyyala@cvshealth.com>
2018-06-21 10:23:23 -0500

    For CMKE-363: use ngrx RecentOrderState in attentionwidget

--- src/app/attention/attention.component.ts
+++ src/app/attention/attention.component.ts
@@ -3,12 +3,14 @@ import {TealiumUtagService} from '../service/utag.service';
 import {ConfigService} from '../service/config.service';
 import {OrderStatusService} from '../order-status/order-status.service';
 import {OrderStatus} from '../order-status/order-status.interface';
-import {ORDER_STATUS_TYPES} from '../order-status/order-status.constants';
+import {ORDER_STATUS_CODES_ATTENTION_ONHOLDS, ORDER_STATUS_TYPES} from '../order-status/order-status.constants';
 import {EccrService} from '../service/eccr.service';
+import {RecentOrdersState} from '../store/recent-orders/recent-orders.reducer';
+import {Observable} from 'rxjs/Observable';
+import {Store} from '@ngrx/store';
+import {RecentOrdersFetch} from '../store/recent-orders/recent-orders.actions';
+import * as _ from 'lodash';
 
-interface AttentionWidgetData {
-  Orders: OrderStatus[];
-}
 
 export enum HOLD_ORDER_INTERACTION {
   TYPE = 3225,
@@ -23,15 +25,18 @@ export enum HOLD_ORDER_INTERACTION {
   styleUrls: ['./attention.component.css']
 })
 export class AttentionComponent implements OnInit {
-  public attentionData: AttentionWidgetData = {Orders: []};
+  public attentionData: RecentOrdersState = {OrdersCount: 0, Orders: []};
 
   public loading = true;
   public ORDER_STATUS_HREF_TEXT = 'View all orders';
+  public recentOrders$: Observable<RecentOrdersState>;
+  public recentOrders: RecentOrdersState;
 
   constructor(private analytics: TealiumUtagService,
               private configSvc: ConfigService,
               private orderStatusService: OrderStatusService,
-              private eccrService: EccrService) {
+              private eccrService: EccrService,
+              private store: Store<any>) {
   }
 
   public getWidgetData() {
@@ -62,7 +67,22 @@ export class AttentionComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getWidgetData();
+    // this.getWidgetData();
+    this.store.dispatch(new RecentOrdersFetch());
+    this.recentOrders$ = this.store.select('recetnOrdersState');
+    this.recentOrders$.subscribe((recentOrders: RecentOrdersState) => {
+      const holdOrders: OrderStatus[] = [];
+      for (const order of recentOrders.Orders) {
+        for (const rx of order.RxList) {
+          if (_.includes(ORDER_STATUS_CODES_ATTENTION_ONHOLDS, rx.StatusReasonCode)) {
+            this.attentionData.Orders.push(Object.assign({RxList: [rx]}, order));
+            // We only care about first Rx onhold per order
+            break;
+          }
+        }
+      }
+      this.loading = false;
+    });
   }
 
   orderClickTag() {
